45b4dc6bc300e7470afe1b71f161049f6fead1f2
NZelaya24 <825123@mystma.org>
2023-03-27 14:46:10 -0500

    3

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #WRITE YOUR CODE HERE
 def addCheck (x, y ,z ):
     if x + y == z:
-        return "true"
+        return True
     else:
-        return "false"
+        return False
 
-addcheck (2,2,4)
+addCheck (2,2,4)
 addCheck (10, 3 ,13)
 addCheck (10, 3 ,30)
